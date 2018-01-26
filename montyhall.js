ba5801a180ab006db8d12d4d9e74052de341350f
simon-kyger <simonkyger@hotmail.com>
2018-01-26 17:56:23 -0500

    restructured rendering

--- montyhall.js
+++ montyhall.js
@@ -16,22 +16,37 @@ let games = function(args){
 	let ret = [];
 
 	for(let i=0; i<args; i++){
-		ret.push({ ['game'+i]: new game});
+		ret.push({ ['game'+(i+1)]: new game});
 	}
 	return ret;
 }
 
-let simulation = games(10);
-console.log(simulation);
+//implementation
 
 
 
 
-
-
+//rendering
 
 document.addEventListener("DOMContentLoaded", function(e) {
-	let p = document.createElement("p");
-	document.body.appendChild(p);
-	p.innerHTML = `Simulator array: ${JSON.stringify(simulation)}`;
+	document.body.appendChild(document.createElement("div")).innerHTML = `
+		<div>Number of Simulations</div>
+		<input id="numsimcount"></input>
+		<input type="radio" id="switch"></input>
+		<label for="switch">Switch</label>
+		<input type="radio" id="dontswitch"></input>
+		<label for="dontswitch">Dont Switch</label>
+		<br>
+		<button id="run">Run Simulation</button>
+		<div>Results: </div>
+		<div id="results">TEST</div>
+		<div>Games array: </div>
+		<div id="gamesarray"></div>
+	`;
+	document.getElementById("run").addEventListener("click", function(){
+		let simcount = document.getElementById("numsimcount").value
+		let simulation = games(simcount);
+		document.getElementById("results").innerHTML = 'asdf';
+		document.getElementById("gamesarray").innerHTML = JSON.stringify(simulation);
+	});
 });
