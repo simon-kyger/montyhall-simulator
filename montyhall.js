9f8d77e25d63858bf640f0bd96c76a4339f23802
simon-kyger <simonkyger@hotmail.com>
2018-01-26 16:35:18 -0500

    Initial commit

---
+++ montyhall.js
@@ -0,0 +1,36 @@
+//setup
+let game = function(){
+	let ret = [{
+			room1: 0
+		},{
+			room2: 0
+		},{
+			room3: 0
+		}
+	];
+	let randindex = Math.floor(Math.random()*(ret.length));
+	ret[randindex][Object.keys(ret[randindex])[0]] = 1;
+	return ret
+}
+let simulator = function(args){
+	let ret = [];
+
+	for(let i=0; i<args; i++){
+		ret.push({ ['game'+i]: new game});
+	}
+	return ret;
+}
+
+let simulation = simulator(50);
+
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function(e) {
+	let p = document.createElement("p");
+	document.body.appendChild(p);
+	p.innerHTML = `Rooms array: ${JSON.stringify(simulation)}`;
+});
